c0e8b962a76eef2d548ce2d3feca8f1f45c06c11
Muhammad Hasaan <m.hasaan1999@gmail.com>
2021-12-23 19:18:13 +0500

    Buyer Portal added

---
+++ Buyer Portal/BigBazaar/BigBazaar/Controllers/TrackOrderController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TrackOrder.Models;
+
+namespace BigBazaar.Controllers
+{
+    public class TrackOrderController : Controller
+    {
+        // GET: TrackOrder
+        // GET: TrackOrder
+        public ActionResult TrackOrder()
+        {
+            string email = TempData["CustomerEmail"] as string;
+            orderDetails obj = new orderDetails { sellerEmail = "", sellerName = "", orderID = "", customerEmail = "", customerName = "", productID = "", productName = "", productLink = "", productPrice = "", productDays = "", shipingAddress = "", totalPrice = 1 };
+            return View();
+        }
+
+        
+    }
+}
+ 
